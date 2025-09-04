03fb580955612c0da1b452c2adbe39f2df00fcc7
Andre Thomas <drethedevjs@gmail.com>
2025-09-04 10:37:46 -0400

    Added about page.

---
+++ app/pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+  <main class="ta-container">
+    <h1 class="text-navy">About Thrive</h1>
+    <p class="text-3xl">
+      At Thrive Micro-School, we believe every child is created with a unique
+      purpose and the ability to learn with joy. Our faith-based program
+      combines academic excellence, life skills, and spiritual growth to nurture
+      confident learners who are prepared for life—not just tests.
+    </p>
+  </main>
+</template>
